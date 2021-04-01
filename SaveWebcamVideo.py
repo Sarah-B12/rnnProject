1a5db011cc38558937d34ad42c119d8ded04c25b
Gal <sgal8@walla.co.il>
2021-04-01 19:38:18 +0300

    update

--- SaveWebcamVideo.py
+++ SaveWebcamVideo.py
@@ -4,18 +4,21 @@ vid_capture = cv2.VideoCapture(0)
 vid_capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 vid_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 vid_cod = cv2.VideoWriter_fourcc(*'XVID')
-output = cv2.VideoWriter("video_data/cam_video.avi", vid_cod, 30.0, (1280,720))
+path = "video_data/cam_video"
 
-counter = 0
-while(True):
-     # Capture each frame of webcam video
-     ret,frame = vid_capture.read()
-     cv2.imshow("My cam video", frame)
-     output.write(frame)
-     counter = counter + 1
-     # Close and break the loop after pressing "x" key
-     if cv2.waitKey(1) & counter == 150:
-         break
+for x in range (14,20):
+     counter = 0
+     output = cv2.VideoWriter(path+str(x)+".avi", vid_cod, 30.0, (1280, 720))
+     while(True):
+          # Capture each frame of webcam video
+          ret,frame = vid_capture.read()
+          cv2.imshow("My cam video", frame)
+          output.write(frame)
+          counter = counter + 1
+          # Close and break the loop after pressing "x" key
+          if cv2.waitKey(1) & counter == 150:
+              break
+x = x + 1
 
 
 # close the already opened camera
