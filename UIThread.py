40e728845a87894778db2a30301a4af6d7e7288f
Gal <sgal8@walla.co.il>
2021-05-03 17:42:38 +0300

    update UI to connect to the predict folder

---
+++ UIThread.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import tkinter as tk
+from tkinter import ttk
+from random import randint
+import shutil
+import logging
+import threading
+import time
+
+fight_videos = ["here", "be"], ["the", "videos"]
+
+
+UI = tk.Tk()
+label = tk.Label(UI, text="Fight Videos", font=("Arial", 30)).grid(row=0, columnspan=3)
+cols = ('Name of video', 'Why the video is violent')
+listBox = ttk.Treeview(UI, columns=cols, show='headings')
+for col in cols:
+    listBox.heading(col, text=col)
+listBox.grid(row=1, column=0, columnspan=2)
+
+def Exit(my_thread):
+    my_thread.do_run = False #if 'do_run' variable in the thread is False - the thread will be kill
+    sys.exit("Bye Bye :)") #close program
+
+
+def UpdateList():
+
+    global fight_videos
+    data_dir = 'video_forBD/Fight'
+    t = threading.currentThread()
+    while getattr(t, "do_run", True): #if 'do_run' == True - the thread is alive
+        files = os.listdir(data_dir)
+        if files:  # if folder Fight is not empty
+
+            current_vid = files[0]
+
+            #update the array with new values
+            new_num1 = str(randint(0,1000))
+            new_num2 = str(randint(0,1000))
+            current_vid_results = ((new_num1, new_num2),)
+
+
+            for i, (name, score) in enumerate(current_vid_results, start=1):
+                listBox.insert("", "end", values=(name, score))
+
+            shutil.move(os.path.join(data_dir, current_vid), 'video_forBD/Fight_Tested')
+
+    #   showScores = tk.Button(UI, text="Continue", command=UI.destroy).grid()
+    #    UI.after(2000, UpdateList)  # run itself again after 2000 ms
+
+my_tread = threading.Thread(target=UpdateList)
+my_tread.start()
+closeButton = tk.Button(UI, text="Close", width=15, command=lambda: Exit(my_tread)).grid(row=4, column=1)
+UI.mainloop()
